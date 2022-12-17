f6c64963ef766e03d929655a88708027cbc5b9eb
= <=>
2022-12-17 14:20:39 +1100

    Changed to Manifest v2 and added Mozilla ID to manifest.json for extension signing, updated js in priceRange.js to handle $1m+ properties

--- priceRange.js
+++ priceRange.js
@@ -1,1 +1,2 @@
+
 document.getElementsByClassName("property-price property-info__price")[0].innerHTML += ` <br />Price Range: ${document.documentElement.innerHTML.match(/\d+(\.\d+)?(m|k)_\d+(\.\d+)?(m|k)/)[0].replace("_", " - ")} 🤫`
